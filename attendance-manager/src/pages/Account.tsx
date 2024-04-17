a4f43d6c12b85d5d47d6b6db402569c9cc8cbaeb
Ghaadyy <ghadyyi@gmail.com>
2024-04-17 20:05:16 +0300

    Fixed select input in user form

--- attendance-manager/src/pages/Account.tsx
+++ attendance-manager/src/pages/Account.tsx
@@ -56,13 +56,13 @@ function Account() {
       ),
     });
 
-    if(res.ok){
+    if (res.ok) {
       toast.success("Changes saved", {
-        toastId: res.status
+        toastId: res.status,
       });
     } else {
       toast.error("Could not save changes", {
-        toastId: res.status
+        toastId: res.status,
       });
     }
 
@@ -224,20 +224,12 @@ function Account() {
 
             <div className="sm:col-span-9">
               <select
-                data-hs-select='{
-                  "placeholder": "Select option...",
-                  "toggleTag": "<button type=\"button\"></button>",
-                  "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1]",
-                  "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300",
-                  "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100",
-                  "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 size-3.5 text-blue-600\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>",
-                  "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"flex-shrink-0 size-3.5 text-gray-500\" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
-                }'
-                className="hidden"
                 id="blood-type"
+                {...register("bloodtype")}
                 defaultValue={user?.bloodType}
+                className="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
               >
-                <option value="">Choose</option>
+                <option value="">Open this select menu</option>
                 <option>A+</option>
                 <option>A</option>
                 <option>A-</option>
